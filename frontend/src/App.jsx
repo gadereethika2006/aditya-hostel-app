9e0d36343ad435468f1304c4e084d231dff67606
gadereethika2006 <gade.reethika2006@gmail.com>
2026-02-10 13:18:00 +0530

    test render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import StudentDashboard from "./pages/StudentDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
